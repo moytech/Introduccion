7e4f2e199af7d5113f27ed2dfa9a919da5c8a755
moitech <moi90tech@gmail.com>
2024-06-20 12:43:33 -0600

    Se creó una cuenta en git hub

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,6 +1,8 @@
 #Este Hola Mundo es para usar en un ejemplo de git
 #Cusrsos de Udemy
 
+#Se hicieron las anotaciones en la libreta
+
 print("Hola Mundo")
 
 # Estes es el primer programa para git
@@ -16,4 +18,8 @@ print("Se agrega texto después de crear una rama")
 print("Se unio la ramamoi a la master")
 
 #Se eliminó rama ramamoi
+
+#Se crea cuenta en github 
+print("Se crea la cuenta en github")
+
 
